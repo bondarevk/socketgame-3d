f400a9f899ebf9fd8e773c1215d06b4c230b7831
LockDownRU <kirill300300@gmail.com>
2017-02-24 14:00:54 +0300

    IO Events

--- public/js/Game/IO.js
+++ public/js/Game/IO.js
@@ -7,15 +7,30 @@ let IO = {
     init: () => {
         this.socket = io.connect();
 
-        // Подключился
+        IO.initEvents();
+    },
+
+    initEvents: () => {
+
+        // Подключился к серверу
         this.socket.on('connect', () => {
             Game.UI.textStatus.innerText = "Online";
         });
 
-        // Отключился
+        // Отключился от сервера
         this.socket.on('disconnect', () => {
             Game.UI.textStatus.innerText = "Offline";
         });
+
+
+        // Подготовка клиента
+        this.socket.on('clientRunUp', (packet) => {
+
+
+            Input.setupKeys(packet.inputReq);
+
+        });
+
     }
 
 };
