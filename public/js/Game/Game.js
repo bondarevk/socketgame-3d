e88cc0c7f4567e6b46b5a27f9217e9f4f4035e2e
LockDownRU <kirill300300@gmail.com>
2017-02-24 12:56:42 +0300

    Camera

--- public/js/Game/Game.js
+++ public/js/Game/Game.js
@@ -6,7 +6,9 @@ let Game = {
 
     time: 0,
 
-    camera: undefined,
+    cameraControls: {
+        camera: undefined
+    },
     scene: undefined,
     renderer: undefined,
 
@@ -20,37 +22,44 @@ let Game = {
     },
 
     initGraphics: () => {
+        // Элемент игры
         Game.container = document.getElementById('gameContainer');
-        Game.camera = new THREE.PerspectiveCamera(60, Game.container.clientWidth / Game.container.clientHeight, 0.2, 2000);
 
+        // Сцена
         Game.scene = new THREE.Scene();
         Game.clock = new THREE.Clock();
 
-        Game.camera.position.x = -7;
-        Game.camera.position.y = 5;
-        Game.camera.position.z = 8;
+        // Камера
+        Game.cameraControls.camera = new THREE.PerspectiveCamera(60, Game.container.clientWidth / Game.container.clientHeight, 0.2, 2000);
+        Game.cameraControls.camera.position.x = -7;
+        Game.cameraControls.camera.position.y = 5;
+        Game.cameraControls.camera.position.z = 8;
 
+        // Рендерер
         Game.renderer = new THREE.WebGLRenderer();
         Game.renderer.setClearColor(0xbfd1e5);
         Game.renderer.setPixelRatio(window.devicePixelRatio);
         Game.renderer.setSize(Game.container.clientWidth, Game.container.clientHeight);
         Game.renderer.shadowMap.enabled = true;
 
+        // Загрузчик текстур
         Game.textureLoader = new THREE.TextureLoader();
 
+        // Источник света
         let ambientLight = new THREE.AmbientLight(0x404040);
         Game.scene.add(ambientLight);
 
-
+        //
         Game.container.innerHTML = "";
         Game.container.appendChild(Game.renderer.domElement);
 
+        // Счетчик фпс
         Game.stats = new Stats();
         Game.stats.domElement.style.position = 'absolute';
         Game.stats.domElement.style.top = '0px';
         Game.container.appendChild(Game.stats.domElement);
 
-
+        // Ресайз игры
         window.addEventListener('resize', Game.onWindowResize, false);
     },
 
@@ -82,20 +91,20 @@ let Game = {
 
     render: () => {
         let deltaTime = Game.clock.getDelta();
-        Game.renderer.render(Game.scene, Game.camera);
+        Game.renderer.render(Game.scene, Game.cameraControls.camera);
         Game.time += deltaTime;
     },
 
 
     onWindowResize: () => {
-        Game.camera.aspect = Game.container.clientWidth / Game.container.clientHeight;
-        Game.camera.updateProjectionMatrix();
+        Game.cameraControls.camera.aspect = Game.container.clientWidth / Game.container.clientHeight;
+        Game.cameraControls.camera.updateProjectionMatrix();
         Game.renderer.setSize(Game.container.clientWidth, Game.container.clientHeight);
     }
 };
 
-if (Detector.webgl === false) {
-    Detector.addGetWebGLMessage();
+if (Detector.webgl === false || Detector.havePointerLock === false) {
+    Detector.addErrorMessage();
     $('#game').hide();
 } else {
     Game.init();
--- public/js/Game/Utils/Detector.js
+++ public/js/Game/Utils/Detector.js
@@ -3,14 +3,14 @@
  * @author mr.doob / http://mrdoob.com/
  */
 
-var Detector = {
+let Detector = {
 
 	canvas: !! window.CanvasRenderingContext2D,
 	webgl: ( function () {
 
 		try {
 
-			var canvas = document.createElement( 'canvas' ); return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) );
+			let canvas = document.createElement( 'canvas' ); return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) );
 
 		} catch ( e ) {
 
@@ -21,10 +21,11 @@ var Detector = {
 	} )(),
 	workers: !! window.Worker,
 	fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+    havePointerLock: 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document,
 
-	getWebGLErrorMessage: function () {
+	getErrorMessage: () => {
 
-		var element = document.createElement( 'div' );
+		let element = document.createElement( 'div' );
 		element.id = 'webgl-error-message';
 		element.style.fontFamily = 'monospace';
 		element.style.fontSize = '13px';
@@ -36,7 +37,7 @@ var Detector = {
 		element.style.width = '400px';
 		element.style.margin = '5em auto 0';
 
-		if ( ! this.webgl ) {
+		if ( Detector.webgl === false ) {
 
 			element.innerHTML = window.WebGLRenderingContext ? [
 				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
@@ -46,26 +47,23 @@ var Detector = {
 				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
 			].join( '\n' );
 
+		} else if ( Detector.havePointerLock === false ) {
+            element.innerHTML = 'Ваш браузер не поддерживает Pointer Lock API';
 		}
 
 		return element;
-
 	},
 
-	addGetWebGLMessage: function ( parameters ) {
+	addErrorMessage: () => {
+		let parent, id, element;
 
-		var parent, id, element;
+		parent = document.body;
+		id = 'oldie';
 
-		parameters = parameters || {};
-
-		parent = parameters.parent !== undefined ? parameters.parent : document.body;
-		id = parameters.id !== undefined ? parameters.id : 'oldie';
-
-		element = Detector.getWebGLErrorMessage();
+		element = Detector.getErrorMessage();
 		element.id = id;
 
 		parent.insertBefore(element, parent.firstChild);
-
 	}
 
 };
